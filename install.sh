bfe1fe192ed0b4199386bf5882fc85d0a8b3b481
alexchenx <alex2436943>
2019-05-10 11:14:21 +0800

    change run mode when execute install_mysql.sh to source install_mysql.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,20 @@ echo "4. Install Wordpress"
 echo ""
 read -p "Please input number to install: " input_num
 
-if [ ${input_num} == 0 ]; then
-	./install_mysql.sh
-	./install_php.sh
-	./install_nginx.sh
-	./install_wordpress.sh
-elif [ ${input_num} == 1 ]; then
-	./install_mysql.sh
-elif [ ${input_num} == 2 ]; then
-	./install_php.sh
-elif [ ${input_num} == 3 ]; then
-	./install_nginx.sh
-elif [ ${input_num} == 4 ]; then
-	./install_wordpress.sh
-else
-	echo "Don't have this option."
-fi
+case $input_num in
+	0)
+		source install_mysql.sh
+		sh install_php.sh
+		sh install_nginx.sh
+		sh install_wordpress.sh
+	1)
+		source install_mysql.sh
+	2)
+		sh install_php.sh
+	3)
+		sh install_nginx.sh
+	4)
+		sh install_wordpress.sh
+	*)
+		echo "Don't have this option."
+esac
