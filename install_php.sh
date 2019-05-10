76f36db840cd7340d97a3be0d50e14a9eb7e6a32
alexchenx <alex2436943>
2019-05-10 23:48:51 +0800

    fix install php bug

--- install_php.sh
+++ install_php.sh
@@ -7,12 +7,21 @@ mkdir -p /data/{software,app}
 php_home="/data/app/php"
 mysql_home="/data/app/mysql"
 
+install_status_flag=0
 cd /data/software/
 wget https://qooco-software.oss-cn-beijing.aliyuncs.com/php-7.3.5.tar.gz
 tar -zxvf php-7.3.5.tar.gz
 cd php-7.3.5
 ./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl
+install_status_flag=$?
+if [ $install_status_flag !=0 ]; then
+	exit
+fi
 make && make install
+install_status_flag=$?
+if [ $install_status_flag !=0 ]; then
+	exit
+fi
 cp ${php_home}/etc/php-fpm.conf.default ${php_home}/etc/php-fpm.conf 
 cp ${php_home}/etc/php-fpm.d/www.conf.default ${php_home}/etc/php-fpm.d/www.conf
 cp /data/software/php-7.3.5/php.ini-production ${php_home}/lib/php.ini
