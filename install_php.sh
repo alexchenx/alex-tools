c675ae4852bb4a05e3511c004544526f9f5eefb8
alexchenx <alex2436943>
2019-05-11 17:00:32 +0800

    php support gd

--- install_php.sh
+++ install_php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description: Install PHP7.3.5
 
-yum install -y wget gcc gcc-c++ libxml2 libxml2-devel autoconf openssl openssl-devel
+yum install -y wget gcc gcc-c++ libxml2 libxml2-devel autoconf openssl openssl-devel libpng libpng-devel
 mkdir -p /data/{software,app}
 
 php_home="/data/app/php"
@@ -21,10 +21,15 @@ if [ -d /data/app/php ]; then
         rm -rf /data/app/php
 fi
 
+id www
+if [ $? != 0 ]; then
+	useradd www
+fi
+
 wget https://qooco-software.oss-cn-beijing.aliyuncs.com/php-7.3.5.tar.gz
 tar -zxvf php-7.3.5.tar.gz
 cd php-7.3.5
-./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl
+./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl --with-gd
 make && make install
 cp ${php_home}/etc/php-fpm.conf.default ${php_home}/etc/php-fpm.conf 
 cp ${php_home}/etc/php-fpm.d/www.conf.default ${php_home}/etc/php-fpm.d/www.conf
