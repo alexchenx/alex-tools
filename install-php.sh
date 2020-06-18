ada15c584df0a697c0289615334e8b0fa1dce8f4
alexchenx <chenxie@aliyun.com>
2020-06-18 14:42:14 +0800

    add install curl

--- install-php.sh
+++ install-php.sh
@@ -21,15 +21,29 @@ if [ -d /data/app/php ]; then
         rm -rf /data/app/php
 fi
 
+
+# Install curl
+wget https://curl.haxx.se/download/curl-7.70.0.tar.gz
+tar -zxvf curl-7.70.0.tar.gz
+cd curl-7.70.0.tar.gz
+./configure --prefix=/data/app/curl
+make && make install
+
+if [ $? != 0 ]; then
+	echo "curl install failed."
+	exist
+fi
+
+
+# Install php
 id www
 if [ $? != 0 ]; then
 	useradd www
 fi
-
 wget https://www.php.net/distributions/php-7.3.19.tar.gz
 tar -zxvf php-7.3.19.tar.gz
 cd php-7.3.19
-./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl --with-gd
+./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl --with-gd --with-curl=/data/app/curl
 make && make install
 cp ${php_home}/etc/php-fpm.conf.default ${php_home}/etc/php-fpm.conf 
 cp ${php_home}/etc/php-fpm.d/www.conf.default ${php_home}/etc/php-fpm.d/www.conf
