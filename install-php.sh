f9a91b871ed3834fc198f7e870f45697e2aff2d8
Alex Chen <chenxie@aliyun.com>
2020-06-19 22:53:29 +0800

    Update install-php.sh

--- install-php.sh
+++ install-php.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-# Description: Install PHP7.3.5
+# Description: Install PHP7.3.19
 
 yum install -y wget gcc gcc-c++ libxml2 libxml2-devel autoconf openssl openssl-devel libpng libpng-devel
 mkdir -p /data/{software,app}
 
-php_home="/data/app/php"
-
 cd /data/software/
+
+# Install curl
+if [ -f /data/software/curl-7.70.0.tar.gz ]; then
+        echo "/data/software/curl-7.70.0.tar.gz is exist, delete it."
+        rm -rf /data/software/curl-7.70.0.tar.gz
+fi
+wget https://curl.haxx.se/download/curl-7.70.0.tar.gz
+tar -zxvf curl-7.70.0.tar.gz
+cd curl-7.70.0
+./configure --prefix=/data/app/curl
+make && make install
+
+if [ $? != 0 ]; then
+	echo "curl install failed."
+	exit
+fi
+
+
+# Install php
+php_home="/data/app/php"
 
 if [ -f /data/software/php-7.3.19.tar.gz ]; then
         echo "/data/software/php-7.3.19.tar.gz is exist, delete it."
@@ -21,21 +39,6 @@ if [ -d /data/app/php ]; then
         rm -rf /data/app/php
 fi
 
-
-# Install curl
-wget https://curl.haxx.se/download/curl-7.70.0.tar.gz
-tar -zxvf curl-7.70.0.tar.gz
-cd curl-7.70.0.tar.gz
-./configure --prefix=/data/app/curl
-make && make install
-
-if [ $? != 0 ]; then
-	echo "curl install failed."
-	exist
-fi
-
-
-# Install php
 id www
 if [ $? != 0 ]; then
 	useradd www
@@ -45,6 +48,10 @@ tar -zxvf php-7.3.19.tar.gz
 cd php-7.3.19
 ./configure --prefix=${php_home} --enable-fpm --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl --with-gd --with-curl=/data/app/curl
 make && make install
+if [ $? -ne 0 ]; then
+	echo "make && make install failed."
+	exit
+fi
 cp ${php_home}/etc/php-fpm.conf.default ${php_home}/etc/php-fpm.conf 
 cp ${php_home}/etc/php-fpm.d/www.conf.default ${php_home}/etc/php-fpm.d/www.conf
 cp /data/software/php-7.3.5/php.ini-production ${php_home}/lib/php.ini
