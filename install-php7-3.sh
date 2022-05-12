bd77e8c0ec77f4ff81b20a456748032bb608234b
Alex Chen <chenxie@aliyun.com>
2022-05-12 22:27:02 +0800

    Upgrade to php-7.3.24

--- install-php7-3.sh
+++ install-php7-3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Description: Install PHP7.3.19
+# Description: Install PHP7.3.24
 
 yum install -y wget gcc gcc-c++ libxml2 libxml2-devel autoconf openssl openssl-devel libpng libpng-devel
 mkdir -p /data/{software,app}
@@ -19,7 +19,7 @@ install_curl(){
                 echo "/data/app/curl is exist, delete it."
                 rm -rf /data/app/curl
         fi
-        wget https://curl.haxx.se/download/curl-7.70.0.tar.gz
+        wget https://curl.haxx.se/download/curl-7.70.0.tar.gz --no-check-certificate
         tar -zxvf curl-7.70.0.tar.gz
         cd curl-7.70.0
         ./configure --prefix=/data/app/curl
@@ -76,13 +76,13 @@ install_php(){
         # Install php
         php_home="/data/app/php"
 
-        if [ -f /data/software/php-7.3.19.tar.gz ]; then
-                echo "/data/software/php-7.3.19.tar.gz is exist, delete it."
-                rm -rf /data/software/php-7.3.19.tar.gz
+        if [ -f /data/software/php-7.3.24.tar.gz ]; then
+                echo "/data/software/php-7.3.24.tar.gz is exist, delete it."
+                rm -rf /data/software/php-7.3.24.tar.gz
         fi
-        if [ -d /data/software/php-7.3.19 ]; then
-                echo "/data/software/php-7.3.19 is exist, delete it."
-                rm -rf /data/software/php-7.3.19
+        if [ -d /data/software/php-7.3.24 ]; then
+                echo "/data/software/php-7.3.24 is exist, delete it."
+                rm -rf /data/software/php-7.3.24
         fi
         if [ -d /data/app/php ]; then
                 echo "/data/app/php is exist, delete it."
@@ -95,9 +95,9 @@ install_php(){
         fi
         cd /data/software/
         yum install -y freetype-devel
-        wget https://www.php.net/distributions/php-7.3.19.tar.gz
-        tar -zxvf php-7.3.19.tar.gz
-        cd php-7.3.19
+        wget https://www.php.net/distributions/php-7.3.24.tar.gz
+        tar -zxvf php-7.3.24.tar.gz
+        cd php-7.3.24
         ./configure --prefix=${php_home} --enable-zip --enable-fpm --enable-mysqlnd --enable-mbstring --enable-pcntl --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zlib --with-openssl --with-gd --with-curl=/data/app/curl --with-php-config=/data/app/php/bin/php-config --with-freetype-dir=/data/app/freetype2 --enable-bcmath
         make && make install
         if [ $? -ne 0 ]; then
@@ -106,7 +106,7 @@ install_php(){
         fi
         cp ${php_home}/etc/php-fpm.conf.default ${php_home}/etc/php-fpm.conf 
         cp ${php_home}/etc/php-fpm.d/www.conf.default ${php_home}/etc/php-fpm.d/www.conf
-        cp /data/software/php-7.3.19/php.ini-production ${php_home}/lib/php.ini
+        cp /data/software/php-7.3.24/php.ini-production ${php_home}/lib/php.ini
         sed -i 's/user = nobody/user = www/' ${php_home}/etc/php-fpm.d/www.conf
         sed -i 's/group = nobody/group = www/' ${php_home}/etc/php-fpm.d/www.conf
 
